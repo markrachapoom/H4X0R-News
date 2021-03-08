adefe1f05e9950f5c52bbed03c3ae882f92d838b
markkypanc <rphanchotong@gmail.com>
2021-03-08 17:20:27 +0700

    Connect API, link WebViews and accomplish the whole project

---
+++ H4X0R-News/Views/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  H4X0R-News
+//
+//  Created by Mark Rachapoom on 8/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(
+                    destination: DetailView(url: post.url),
+                    label: {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
+                    })
+            }
+            .navigationBarTitle("H4X0R News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Hola")
+//]

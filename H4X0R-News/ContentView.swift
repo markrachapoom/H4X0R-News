879f7cfe3b0efde9c480254a82126e0767ee0140
markkypanc <rphanchotong@gmail.com>
2021-03-08 12:58:10 +0700

    Add Identifiable Protocol

--- H4X0R-News/ContentView.swift
+++ H4X0R-News/ContentView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         NavigationView {
-            List {
-                Text("Hello, World!")
-                Text("Test for cell")
+            List(posts) { post in
+                Text(post.title)
             }
             .navigationBarTitle("H4X0R News")
         }
@@ -24,3 +24,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
